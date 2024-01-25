d1d2fad754be4bfab8cd781a896765a3aea433ed
Guilherme Simões Rosa <133579859+gurautnso@users.noreply.github.com>
2024-01-24 21:54:23 -0300

    codigo terminado

--- tic tac toe/__init__.py
+++ tic tac toe/__init__.py
@@ -1,21 +1,27 @@
 from functions import *
 from random import randint
-from time import sleep
 
-victory = j2 = j1 = True
+j2 = j1 = True
+victory = ''
 tabuleiro = {1: ' ', 2: ' ', 3: ' ',
              4: ' ', 5: ' ', 6: ' ',
              7: ' ', 8: ' ', 9: ' '}
 display_board(tabuleiro)
-while victory:
+while True:
     while j1:
         j2 = True
         j = int(input('diga o numero da jogada:'))
         j1 = enter_move(tabuleiro, j, 'O')
-        display_board(tabuleiro)
+    display_board(tabuleiro)
+    victory = victory_for(tabuleiro, 'O')
+    if victory != '':
+        break
     while j2:
         j1 = True
         j = randint(1, 9)
         j2 = enter_move(tabuleiro, j, 'X')
-        display_board(tabuleiro)
-        sleep(5)
+    display_board(tabuleiro)
+    victory = victory_for(tabuleiro, 'X')
+    if victory != '':
+        break
+print(f'{victory} ganhou')
--- tic tac toe/functions/__init__.py
+++ tic tac toe/functions/__init__.py
@@ -7,8 +7,6 @@ def display_board(board):
         if k in [3, 6, 9]:
             print()
     print('+-----------+')
-    #  A função aceita um parâmetro contendo o status atual do tabuleiro
-    #  e imprime no console.
 
 
 def enter_move(board, move, mark):
@@ -17,23 +15,25 @@ def enter_move(board, move, mark):
             board.update({move: mark})
             return False
     return True
-    # A função aceita o status atual do tabuleiro, pergunta ao usuário sobre sua movimentação,
-    # verifica a entrada e atualiza o quadro de acordo com a decisão do usuário.
 
 
-'''def make_list_of_free_fields(board):
-
-     # A função navega no tabuleiro e constrói uma lista de todos os quadrados livres;
-     # a lista consiste em tuplas, enquanto cada tupla é um par de números de linha e coluna.
-
-
-def victory_for(board, sign):
-
-     # A função analisa o estado da placa para verificar se
-     # o jogador que usou 'O's ou 'X's ganhou o jogo
-
-
-def draw_move(board):
-
-     # A função desenha o movimento do computador e atualiza o tabuleiro.
-'''
+def victory_for(board, player):
+    vitorias = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    mark = player
+    jogadas = []
+    for k, v in board.items():
+        if v == mark:
+            jogadas.append(k)
+    jogadas.sort()
+    print(jogadas)
+    consec = 0
+    for c in vitorias:
+        for i in c:
+            for v in jogadas:
+                if v == i:
+                    consec += 1
+                if consec >= 3:
+                    return player
+        consec = 0
+    else:
+        return ''
