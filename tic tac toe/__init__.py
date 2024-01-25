e4fdba8202104fe70b63ba18e7260cbfd8c8fa8c
Guilherme Simões Rosa <133579859+gurautnso@users.noreply.github.com>
2024-01-24 22:21:55 -0300

    Update __init__.py

--- tic tac toe/__init__.py
+++ tic tac toe/__init__.py
@@ -10,7 +10,10 @@ display_board(tabuleiro)
 while True:
     while j1:
         j2 = True
-        j = int(input('diga o numero da jogada:'))
+        try:
+            j = int(input('diga o numero da jogada:'))
+        except ValueError:
+            j = 0
         j1 = enter_move(tabuleiro, j, 'O')
     display_board(tabuleiro)
     victory = victory_for(tabuleiro, 'O')
